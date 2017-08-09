ad4f24cb5e6b4118a5ffcf275139633c131ea506
Luis Rafael Rivera <rafael@bambu-mobile.com>
2017-08-09 18:14:27 -0500

    Rafa

--- js/nav.js
+++ js/nav.js
@@ -2,17 +2,17 @@
    $('#link_1').on('click', function(e) {
 
         e.preventDefault();
-        $("html, body").animate({scrollTop:( $('#Inicio').offset().top - 40) }, 1000);
+        $("html, body").animate({scrollTop:( $('#Inicio').offset().top) }, 1000);
     });
 
         $('#link_2').on('click', function(e) {
         e.preventDefault();
         console.log("loggin2g" , $('#Nosotros').offset().top);
-        $("html, body").animate({scrollTop:( $('#Nosotros').offset().top - 50) }, 1000);
+        $("html, body").animate({scrollTop:( $('#Nosotros').offset().top - 90) }, 1000);
     });
             $('#link_3').on('click', function(e) {
         e.preventDefault();
-        $("html, body").animate({scrollTop:( $('#App').offset().top - 40)}, 1000);
+        $("html, body").animate({scrollTop:( $('#App').offset().top - 100)}, 1000);
     });
     });
  
