dd08702b5d958fa977dc1003e68b870011ab9558
Luis Rafael Rivera <rafael@bambu-mobile.com>
2017-08-09 17:17:40 -0500

    rafa
    falta arreglar el nav

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,19 @@
-    $('#link_1').on('click', function(e) {
+    $(document).ready(function(){
+   $('#link_1').on('click', function(e) {
+
         e.preventDefault();
-        $("html, body").animate({scrollTop: $('#Inicio').offset().top }, 1000);
+        $("html, body").animate({scrollTop:( $('#Inicio').offset().top - 40) }, 1000);
     });
 
         $('#link_2').on('click', function(e) {
         e.preventDefault();
-        $("html, body").animate({scrollTop: $('#Nosotros').offset().top }, 1000);
+        console.log("loggin2g" , $('#Nosotros').offset().top);
+        $("html, body").animate({scrollTop:( $('#Nosotros').offset().top - 400) }, 1000);
     });
 
             $('#link_3').on('click', function(e) {
         e.preventDefault();
-        $("html, body").animate({scrollTop: $('#App').offset().top }, 1000);
+        $("html, body").animate({scrollTop:( $('#App').offset().top - 40)}, 1000);
     });
+    });
+ 
