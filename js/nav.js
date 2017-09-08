e017dae1f4c2259a8c6041f7babdf0c29623f146
Luis Rafael Rivera <30272643+RafaelBambu@users.noreply.github.com>
2017-09-08 15:26:02 -0500

    Rafa

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,40 @@
-    $(document).ready(function(){
-   $('#link_1').on('click', function(e) {
+$(document).ready(function () {
+    $('#link_1').on('click', function (e) {
 
         e.preventDefault();
-        $("html, body").animate({scrollTop:( $('#Inicio').offset().top) }, 1000);
+        $("html, body").animate({ scrollTop: ($('#Inicio').offset().top) }, 1000);
     });
 
-        $('#link_2').on('click', function(e) {
+    $('#link_2').on('click', function (e) {
         e.preventDefault();
-        $("html, body").animate({scrollTop:( $('#Nosotros').offset().top - 100) }, 1000);
+        $("html, body").animate({ scrollTop: ($('#Nosotros').offset().top - 100) }, 1000);
     });
-            $('#link_3').on('click', function(e) {
+    $('#link_3').on('click', function (e) {
         e.preventDefault();
-        $("html, body").animate({scrollTop:( $('#App').offset().top - 100)}, 1000);
+        $("html, body").animate({ scrollTop: ($('#App').offset().top - 100) }, 1000);
     });
+    $('#link_4').on('click', function (e) {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: ($('#Inicio').offset().top) }, 1000);
     });
- 
+    $('#link_5').on('click', function (e) {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: ($('#Nosotros').offset().top -150) }, 1000);
+    });
+    $('#link_6').on('click', function (e) {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: ($('#App').offset().top - 100) }, 1000);
+    });
+    $('#link_7').on('click', function (e) {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: ($('#Inicio').offset().top - 100) }, 1000);
+    });
+    $('#link_8').on('click', function (e) {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: ($('#Nosotros').offset().top - 100) }, 1000);
+    });
+    $('#link_9').on('click', function (e) {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: ($('#App').offset().top - 100) }, 1000);
+    });
+});
